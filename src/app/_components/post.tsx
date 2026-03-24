8c4bd73d658ee8669f98648054679d68e022530a
Aniket Warule <Aniketwarule775@gmail.com>
2026-03-25 03:33:50 +0530

    pre-deployment changes

--- src/app/_components/post.tsx
+++ src/app/_components/post.tsx
@@ -1,50 +1,19 @@
 "use client";
 
-import { useState } from "react";
+import { CircularProgress, Typography } from "@mui/material";
 
 import { api } from "~/trpc/react";
 
 export function LatestPost() {
-  const [latestPost] = api.post.getLatest.useSuspenseQuery();
-
-  const utils = api.useUtils();
-  const [name, setName] = useState("");
-  const createPost = api.post.create.useMutation({
-    onSuccess: async () => {
-      await utils.post.invalidate();
-      setName("");
-    },
-  });
+  const helloQuery = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
+    <div>
+      {helloQuery.isLoading ? (
+        <CircularProgress size={18} />
       ) : (
-        <p>You have no posts yet.</p>
+        <Typography>{helloQuery.data?.greeting ?? "No message"}</Typography>
       )}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          createPost.mutate({ name });
-        }}
-        className="flex flex-col gap-2"
-      >
-        <input
-          type="text"
-          placeholder="Title"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="w-full rounded-full bg-white/10 px-4 py-2 text-white"
-        />
-        <button
-          type="submit"
-          className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-          disabled={createPost.isPending}
-        >
-          {createPost.isPending ? "Submitting..." : "Submit"}
-        </button>
-      </form>
     </div>
   );
 }
